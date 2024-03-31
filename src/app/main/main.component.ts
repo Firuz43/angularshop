3057598034e0368bfa616fe452bcf17c94bcb902
Firuz Chamarov <firuz1199@mail.ru>
2024-03-31 22:37:43 +0400

    navbar change fix

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ShopServiceService } from '../service/shop-service.service';
-import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.css',
-    imports: [NavbarComponent]
+    imports: []
 })
 export class MainComponent {
 
