df1495728abb97211af4f4b52105bd165638e67a
Firuz Chamarov <firuz1199@mail.ru>
2024-04-03 21:10:39 +0400

    creating buying page

---
+++ src/app/buying-page/buying-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buying-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './buying-page.component.html',
+  styleUrl: './buying-page.component.css'
+})
+export class BuyingPageComponent {
+
+}
