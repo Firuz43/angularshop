82ffb69f46e0e5d37a80322cc3d588a58ead1d0a
Firuz Chamarov <firuz1199@mail.ru>
2024-04-04 14:23:22 +0400

    adding login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: 'notes', component: MainComponent},
-    {path: 'navbar', component: NavbarComponent}
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'login', component: LoginComponent}
 ];
